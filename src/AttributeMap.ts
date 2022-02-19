339f2980218424e2c90f4220a12ffbc598c72d85
15017662461 <15017662461@163.com>
2022-02-19 20:52:43 +0800

    添加了自己的注释

--- src/AttributeMap.ts
+++ src/AttributeMap.ts
@@ -6,6 +6,13 @@ interface AttributeMap {
 }
 
 namespace AttributeMap {
+  /**
+   *
+   * @param a 对象a
+   * @param b 对象b
+   * @param keepNull
+   * @returns b的所有内容和a中b没有的，即用b覆盖掉a中的内容
+   */
   export function compose(
     a: AttributeMap = {},
     b: AttributeMap = {},
@@ -34,6 +41,12 @@ namespace AttributeMap {
     return Object.keys(attributes).length > 0 ? attributes : undefined;
   }
 
+  /**
+   *
+   * @param a 对象a
+   * @param b 对象b
+   * @returns b中与a不同的,a有b没有返回null
+   */
   export function diff(
     a: AttributeMap = {},
     b: AttributeMap = {},
@@ -55,6 +68,12 @@ namespace AttributeMap {
     return Object.keys(attributes).length > 0 ? attributes : undefined;
   }
 
+  /**
+   *
+   * @param attr 对象a
+   * @param base 对象b
+   * @returns ab都有的用b的，a有b没有的返回null，a没有b有的不返回
+   */
   export function invert(
     attr: AttributeMap = {},
     base: AttributeMap = {},
@@ -74,6 +93,13 @@ namespace AttributeMap {
     }, baseInverted);
   }
 
+  /**
+   *
+   * @param a 对象a
+   * @param b 对象b
+   * @param priority 优先级
+   * @returns 返回b,priority为true则返回b中除了a中有的
+   */
   export function transform(
     a: AttributeMap | undefined,
     b: AttributeMap | undefined,
